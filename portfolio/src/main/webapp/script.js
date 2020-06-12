306dce9469e58b5fcce16ff0c5633647bd988c26
Karol Gutierrez <kjgs.22@gmail.com>
2020-06-12 13:24:24 -0500

    Add newline

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -50,6 +50,7 @@ function changeCat() {
   const catImage = document.getElementById('cat-image');
   catImage.src = src;
 }
+
 // Loads /data content from server and displays it to the web page.
 function showHelloMessage() {
   fetch('/data').then(response => response.text()).then((message) => {
